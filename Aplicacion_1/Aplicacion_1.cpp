3bf26bc4a49ef629f81eb3051c9fad79a5855eef
pablofloresmolina01 <A01797281@tec.mx>
2026-01-26 15:03:00 -0600

    Add files via upload

---
+++ Aplicacion_1/Aplicacion_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <omp.h>
+#include <cstdlib>
+
+#define N 5000
+#define chunk 100
+#define mostrar 10
+
+void imprimeArreglo(float* d);
+
+int main()
+{
+	std::cout << "Sumando Arreglos en Paralelo!\n";
+	float a[N], b[N], c[N];
+	int i;
+
+	for (i = 0; i < N; i++)
+	{
+		a[i] = i * 10;
+		b[i] = (i + 3) * 3.7;
+	}
+	int pedazos = chunk;
+
+	#pragma omp parallel for \
+	shared(a,b,c,pedazos) private(i) \
+	schedule(static, pedazos)
+
+	for (i = 0; i < N; i++)
+		c[i] = a[i] + b[i];
+
+	std::cout << " Imprimiendo los primeros " << mostrar << " valores de arreglo a: " << std::endl;
+	imprimeArreglo(a);
+	std::cout << " Imprimiendo los primeros " << mostrar << " valores de arreglo b: " << std::endl;
+	imprimeArreglo(b);
+	std::cout << " Imprimiendo los primeros " << mostrar << " valores de arreglo c: " << std::endl;
+	imprimeArreglo(c);
+
+	system("pause");
+	return 0;
+
+}
+
+void imprimeArreglo(float* d)
+{
+	for (int x = 0; x < mostrar; x++)
+		std::cout << d[x] << " - ";
+	std::cout << std::endl;
+}
